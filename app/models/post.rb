85214dba7af3aff4454c3f118a2548e2d2ca656a
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-03-01 16:50:01 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
+# class ClickBait < ActiveModel::Validator
+#   def validate(record)
+#     good = Regexp.union([/Won\'t\ Believe/, /Secret/, /Top\ \d*/, /Guess/])
+#     unless record.title.match(good)
+#       record.errors[:title] << 'Need something more clickbaity.'
+#     end
+#   end
+# end
+
 class Post < ActiveRecord::Base
+  # include ActiveModel::Validations
+  # validates_with ClickBait
+  validates :title, {presence: true}
+  validate :clickbait
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+  def clickbait
+    good = Regexp.union([/Won\'t\ Believe/, /Secret/, /Top\ \d*/, /Guess/])
+    if title.present? && !title.match(good)
+      errors.add(:title, 'Need something more clickbaity.')
+    end
+  end
 end
